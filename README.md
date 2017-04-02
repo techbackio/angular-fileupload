d832b371789010f561edb40ead78df1b3dd20ce4
Tanay Pratap <tanay@Tanays-MacBook-Air.local>
2017-04-02 10:22:21 +0530

    fix server for static files

--- README.md
+++ README.md
@@ -5,3 +5,23 @@ This project is a demo for doing file upload with Angular JS, with a Express Bac
 ng-file-upload </a>which solves this issue.
 
 On the other end, Express doesn't support multipart form handling out of the box anymore. Multer is being used here to handle the file upload. It is also configured to rename the file and save it at specified location.
+
+### Start the project
+Install the dependencies:
+```
+npm install
+bower install
+```
+
+Start the dev server:
+```
+npm start
+```
+
+For production usage:
+```
+npm install
+bower install
+```
+
+####  For any querires, send your requests to tanay.mit@gmail.com
--- server.js
+++ server.js
@@ -3,14 +3,13 @@ const multer = require('multer')
 const path = require('path')
 const app = express()
 
-
+app.use('/static', express.static('bower_components'))
 
 var storage = multer.diskStorage({
   destination : function (req, file, cb) {
     cb(null, __dirname + '/public/uploads/')
   },
   filename : (req, file, cb) => {
-    console.log("In here...", file)
     const filename = file.originalname.split(".")
     cb(null, `${filename[0]}_${Date.now()}.${filename[1]}`)
   }
@@ -22,15 +21,16 @@ var upload = multer({
 
 
 app.post('/upload', upload.any(), function(req, res) {
-  console.log("Printing req.files...", req.files)
   res.json({ success: true, fileName: req.files[0].originalname,
               savedfileName : req.files[0].filename })
 })
 
 
-app.get('*', function(req, res) {
+app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname + '/index.html'))
 })
 
 
-app.listen(3000)
+app.listen(3000,() => {
+  console.log("Server is listening at port 3000")
+})
